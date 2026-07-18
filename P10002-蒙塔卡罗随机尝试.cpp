5dbea845f7a39bae5ccef7546f4493f0061dd475
Yawn_Lento <2665002182@qq.com>
2026-07-18 21:39:51 +0800

    Update P10002-蒙塔卡罗随机尝试.cpp

--- P10002-蒙塔卡罗随机尝试.cpp
+++ P10002-蒙塔卡罗随机尝试.cpp
@@ -1,8 +1,8 @@
-﻿#include <iostream>
+#include <iostream>
 #include <cstdlib>
 #include <ctime>
 using namespace std;
-int rand01() 
+int rand01()
 {
 	return rand() % 2;
 }
@@ -16,7 +16,7 @@ int distance(point A, point B)
 }
 bool cmp(point m, point n)
 {
-	return (m.x ==n.x && m.y ==n.y);
+	return (m.x == n.x && m.y == n.y);
 }
 int main()
 {
@@ -24,11 +24,12 @@ int main()
 	point horse, end;
 	static point path[399][39];
 	cin >> end.x >> end.y >> horse.x >> horse.y;
-	point temp;  int valid = 0; int fork; int flag=0; int count = 0;
-	while( count<399)
+	point temp;  int valid = 0; int fork; int flag = 0; int count = 0;
+	int attempt = 0;
+	while (attempt < 10000)
 	{
-		temp.x = temp.y = 0; int step = 0; 
-		while (temp.x <= end.x && temp.y <= end.y&&!cmp(temp, end))
+		temp.x = temp.y = 0; int step = 0;
+		while (temp.x <= end.x && temp.y <= end.y && !cmp(temp, end))
 		{
 			fork = rand01();
 			int tempx = temp.x, tempy = temp.y;
@@ -36,34 +37,39 @@ int main()
 			{
 				temp.x = temp.x + fork, temp.y = temp.y + 1 - fork;
 			}
-			else if (temp.x == end.x && temp.y < end.y)			
-				temp.y++;				
-			else			
+			else if (temp.x == end.x && temp.y < end.y)
+				temp.y++;
+			else
 				temp.x++;
 			if (distance(temp, horse) != 5 && !cmp(temp, horse))
 			{
 				path[count][step] = temp;
-				for (int i = 0; i < count; i++)
-				{
-					if (cmp(path[i][step], temp))
-						flag++;
-				}
 				step++;
 			}
-			else if (tempx < end.x && tempy < end.y)
-				temp.x--, temp.y--;
 			else
 			{
 				flag = 999;
 				break;
 			}
 		}
+		for (int i = 0; i < count; i++)
+		{
+			for (int j = 0; j < step; j++)
+			{
+				if (cmp(path[count][j], path[i][j]))
+					flag++;
+			}
+			if (flag < step)
+				flag = 0;
+		}
 		if (flag < end.x + end.y - 1 && cmp(temp, end))
 		{
+
 			valid++;
 			count++;
 		}
 		flag = 0;
+		attempt++;
 	}
-	cout << valid<<endl;
-}//本地跑总是超时，只能借鉴一下模型思路
+	cout << valid << endl;
+}
