abd43c2f4d8a58d2c30aa108f9ca088c8d8992ea
Yawn_Lento <2665002182@qq.com>
2026-07-22 10:12:02 +0800

    Add files via upload

---
+++ P1002-DFS尝试.cpp
@@ -0,0 +1,27 @@
+﻿#include<iostream>
+using namespace std;
+long long path_count=0;
+int n, m, x, y;
+bool if_block(int i,int j)
+{
+	return((i == x && j == y) || i > n || j > m || i < 0 ||j<0 || ((i - x) * (i - x) + (j - y) * (j - y) == 5));
+}
+void DFS(int i, int j)
+{
+	if(if_block(i, j))
+	return;
+	if (i == n && j == m)
+	{
+		path_count++;
+		return;
+	}
+	DFS(i + 1, j);
+	DFS(i, j + 1);
+}
+int main()
+{
+	cin >> n >> m >> x >> y;
+	DFS(0, 0);
+	cout << path_count << endl;
+	return 0;
+}
---
+++ P1002-DP-longlong防越界-忽视警告.cpp
@@ -0,0 +1,33 @@
+﻿#include<iostream>
+#pragma warning(disable: 6385)
+using namespace std;
+long long dp[22][22];
+int main()
+{
+	int n, m, x, y;
+	cin >> n >> m >> x >> y;
+	if (m > 20 || n > 20 || x > 20 || y > 20||m < 0 || n <0 || x<0 || y<0)
+		return 0;
+	dp[0][0] = 1;
+	for (int i = 0; i <= n; i++)
+	{
+		for (int j = 0; j <= m; j++)
+		{
+			if (i == 0 && j == 0) 
+				continue;
+			if ((i - x) * (i - x) + (j - y) * (j - y) == 5||(i==x&&j==y))
+			{
+				dp[i][j] = 0;
+				continue;
+			}
+			if (i != 0 && j != 0)
+				dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+			else if(i==0)
+				dp[i][j] = dp[i][j - 1];
+			else
+				dp[i][j] = dp[i-1][j];
+		}
+	}
+	cout << dp[n][m] << endl;
+	return 0;
+}
